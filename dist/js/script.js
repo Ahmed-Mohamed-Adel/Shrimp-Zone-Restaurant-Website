f2591ceeb1a05d11f12eff84c42b17f91b0ec552
Ahmed-Mohamed-Adel <ahmedadelelzero@gmail.com>
2025-03-20 18:05:12 +0200

    Edit files

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,24 +9,26 @@ const shrimpImg = document.getElementById("shrimp-img");
 
 let angel = 0;
 
-setInterval(() => {
+function rotateImage() {
   angel += 1;
   shrimpImg.style.transform = `rotate(${angel}deg)`;
-}, 10);
+  requestAnimationFrame(rotateImage);
+}
 
+rotateImage();
 
 // Search By Filter
-document.getElementById("searchInput").addEventListener("keyup", function(){
+document.getElementById("searchInput").addEventListener("keyup", function () {
   let searchValue = this.value.toLowerCase();
   let menuItems = document.querySelectorAll(".menu-item");
 
-  menuItems.forEach((item)=>{
+  menuItems.forEach((item) => {
     let dishName = item.querySelector("h3").textContent.toLowerCase();
 
-    if(dishName.includes(searchValue)){
-      item.style.display = "flex"
-    } else{
+    if (dishName.includes(searchValue)) {
+      item.style.display = "flex";
+    } else {
       item.style.display = "none";
     }
-  })
-})
+  });
+});
